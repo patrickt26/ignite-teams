2944247008fb1ec6afbf1cfc29dca9e0e1c6f086
Patrick Thomaz <plthomaz@gmail.com>
2023-04-18 14:41:29 -0300

    Styled App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
-import { ThemeProvider } from 'styled-components/native'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { ThemeProvider } from 'styled-components/native';
 
-import { Groups } from '@screens/Groups'
+import { Groups } from '@screens/Groups';
 
-import theme from '@theme/index'
+import { Loading } from '@components/Loading';
+import theme from '@theme/index';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
-  )
+  );
 }
